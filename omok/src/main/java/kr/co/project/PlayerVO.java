ebb996e725eab954d01363195bd44aba19f2d307
doobab <114578342+doobab@users.noreply.github.com>
2023-05-17 17:40:21 +0900

    jsp 수정

---
+++ omok/src/main/java/kr/co/project/PlayerVO.java
@@ -0,0 +1,14 @@
+package kr.co.project;
+
+import lombok.Data;
+
+@Data	
+public class PlayerVO {
+	private int playerPK;
+	private String id;
+	private String password;
+	private String nickname;
+	private int victory;
+	private int total;
+
+}
